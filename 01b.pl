b4f7f2b5cfef4841bdcbef4c5bc8e6fc9dde7ead
Eric Aderhold <eric.aderhold@code.org>
2023-12-01 08:51:26 -0800

    Shorter

--- 01b.pl
+++ 01b.pl
@@ -1,20 +1,12 @@
 %names = (
-  0 => 0, 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9,
   'one' => 1, 'two' => 2, 'three' => 3, 'four' => 4, 'five' => 5,
   'six' => 6, 'seven' => 7, 'eight' => 8, 'nine' => 9
 );
 
 for (<>) {
-  $_ = substr($_, 1) while !/^(0|1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine)/;
-  chop while !/(0|1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine)$/;
+  @matches = map { $names{$_} || $_ } /(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g;
 
-  /^(0|1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine)/;
-  $first = $names{$1};
-
-  /(0|1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine)$/;
-  $last = $names{$1};
-
-  $sum += $first . $last;
+  $sum += $matches[0] . $matches[-1];
 }
 
 print $sum, "\n";
